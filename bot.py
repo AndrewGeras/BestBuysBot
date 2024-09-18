ad344a0314073cb5975cc5c1d3c4928cf611e51a
Andrey Gerasimov <gerasimov.andrey1985@gmail.com>
2024-09-18 20:06:34 +0200

    add config.py

--- bot.py
+++ bot.py
@@ -2,10 +2,13 @@ import asyncio
 
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from config_data.config import Config, load_config
 
 
 async def main():
-    pass
+    config: Config = load_config()
+    bot = Bot(token=config.tg_bot.token, default=DefaultBotProperties(parse_mode='HTML'))
+    dp = Dispatcher()
 
 
 if __name__ == '__main__':
