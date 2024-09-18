dfbd46cbac7016220351261a74818e86fd7679ab
Andrey Gerasimov <gerasimov.andrey1985@gmail.com>
2024-09-18 19:16:43 +0200

    init project

---
+++ bot.py
@@ -0,0 +1,12 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+
+
+async def main():
+    pass
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
